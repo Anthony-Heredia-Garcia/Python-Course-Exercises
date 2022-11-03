9a8d748efde9d7a5d01d04ee42b6fd66f2d3164a
Anthony-Heredia-Garcia <99502244+Anthony-Heredia-Garcia@users.noreply.github.com>
2022-11-03 15:10:14 -0400

    Assignment 2 - working with lists

--- assignment1.py
+++ assignment1.py
@@ -1,1 +1,11 @@
-#Do this assignment tomorrow.
+
+name = input('What is your name? ')
+age = input('What is your age? ')
+
+print('Hello ' +name+'. Your age is '+age+'.')
+
+def decades(age):
+  decade_num = age//10
+  print('You have lived for '+str(decade_num)+' decades.')
+
+decades(int(age))
